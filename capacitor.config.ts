9e5496f1e4af197acf056f4f3437d7e29d265381
AndyMan <josueandy5@gmail.com>
2024-01-06 22:26:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'u.media',
+  appName: 'u-media',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
